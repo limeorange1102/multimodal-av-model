cfe82cfaf1fc2e38b39fbf4b6709a26d018a1985
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-13 16:28:21 +0900

    d

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def main():
     text_dir = "processed_dataset/text"
     wav_dir = "input_videos"
 
-    tokenizer = Tokenizer(vocab_path="utils/tokenizer800.vocab")
+    tokenizer = Tokenizer(vocab_path="input_videos/tokenizer800.vocab")
     sentence_list = build_data_list(json_folder, npy_dir, text_dir, wav_dir)
     train_sent, val_sent = train_test_split(sentence_list, test_size=0.1, random_state=42)
     val_pairs = generate_fixed_pairs(val_sent, n_pairs=500)
