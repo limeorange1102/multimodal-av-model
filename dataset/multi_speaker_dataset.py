dc67e0e97c2a4fb6344493f9d8fc15e600627efb
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-16 06:25:53 +0900

    lip2 delete

--- dataset/multi_speaker_dataset.py
+++ dataset/multi_speaker_dataset.py
@@ -38,27 +38,20 @@ class MultiSpeakerDataset(torch.utils.data.Dataset):
 
         # Load lips
         lip1 = np.load(s1["lip_path"])  # (T1, 27, 2)
-        lip2 = np.load(s2["lip_path"])  # (T2, 27, 2)
         #(128x128)->(64x64)로 resize
         lip1 = np.stack([cv2.resize(frame, (64, 64)) for frame in lip1])
-        lip2 = np.stack([cv2.resize(frame, (64, 64)) for frame in lip2])
 
         # Load labels
         with open(s1["text_path"], "r", encoding="utf-8") as f:
             label1 = self.tokenizer.encode(f.read().strip())
-        with open(s2["text_path"], "r", encoding="utf-8") as f:
-            label2 = self.tokenizer.encode(f.read().strip())
 
         return {
             "audio": mix.astype(np.float32),
             "audio1_raw": a1.astype(np.float32),
             "audio2_raw": a2.astype(np.float32),
             "lip1": lip1.astype(np.float32),
-            "lip2": lip2.astype(np.float32),
             "label1": np.array(label1, dtype=np.int64),
-            "label2": np.array(label2, dtype=np.int64),
             "lip1_len": lip1.shape[0],
-            "lip2_len": lip2.shape[0],
         }
 
 
@@ -104,24 +97,17 @@ class FixedSentencePairDataset(MultiSpeakerDataset):
         mix = mix / (np.max(np.abs(mix)) + 1e-6)
 
         lip1 = np.load(s1["lip_path"])
-        lip2 = np.load(s2["lip_path"])
         #(128x128)->(64x64)로 resize
         lip1 = np.stack([cv2.resize(frame, (64, 64)) for frame in lip1])
-        lip2 = np.stack([cv2.resize(frame, (64, 64)) for frame in lip2])
 
         with open(s1["text_path"], "r", encoding="utf-8") as f:
             label1 = self.tokenizer.encode(f.read().strip())
-        with open(s2["text_path"], "r", encoding="utf-8") as f:
-            label2 = self.tokenizer.encode(f.read().strip())
 
         return {
             "audio": mix.astype(np.float32),
             "audio1_raw": a1.astype(np.float32),
             "audio2_raw": a2.astype(np.float32),
             "lip1": lip1.astype(np.float32),
-            "lip2": lip2.astype(np.float32),
             "label1": np.array(label1, dtype=np.int64),
-            "label2": np.array(label2, dtype=np.int64),
             "lip1_len": lip1.shape[0],
-            "lip2_len": lip2.shape[0],
         }
