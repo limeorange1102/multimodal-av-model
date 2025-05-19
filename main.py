4890cac1ec5bbd0c12a7993841babdcd4d7569ef
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-19 21:55:03 +0900

    test set

--- main.py
+++ main.py
@@ -68,15 +68,22 @@ def main():
     wav_dir = "input_videos"
 
     tokenizer = Tokenizer(vocab_path="input_videos/tokenizer800.vocab")
-    sentence_list = build_data_list(json_folder, npy_dir, text_dir, wav_dir)
-    train_sent, val_sent = train_test_split(sentence_list, test_size=0.1, random_state=42)
-    val_pairs = generate_fixed_pairs(val_sent, n_pairs=500)
+    sentence_list = build_data_list(json_folder, npy_dir, text_dir, wav_dir)    
 
-    train_dataset = RandomSentencePairDataset(train_sent, tokenizer, num_pairs_per_epoch=10000)
+    train_set, temp_set = train_test_split(sentence_list, test_size=0.1, random_state=42)
+    val_set, test_set = train_test_split(temp_set, test_size=0.5, random_state=42)
+    
+    train_dataset = RandomSentencePairDataset(train_set, tokenizer, num_pairs_per_epoch=10000)
+
+    val_pairs = generate_fixed_pairs(val_set, n_pairs=500)
     val_dataset = FixedSentencePairDataset(val_pairs, tokenizer)
+
+    test_pairs = generate_fixed_pairs(test_set, n_pairs=1000)
+    test_dataset = FixedSentencePairDataset(test_pairs, tokenizer)
 
     train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2, collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=collate_fn)
 
     visual_encoder = VisualEncoder(
         pretrained_path="weights/Video_only_model.pt",
