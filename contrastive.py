3aabc49fc3205c5f2f42540f1f772a3ab2e916aa
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-30 06:47:34 +0900

    tlqkftlqkf

--- contrastive.py
+++ contrastive.py
@@ -5,41 +5,31 @@ TEMPERATURE = 0.07
 WEIGHT_STRONG_POS = 1.0
 WEIGHT_WEAK_POS = 0.3
 
-def contrastive_loss_with_mask(audio_feat, mask):
-    """
-    audio_feat: [B, T, D]
-    mask: [B, T] with values in {0, 1, 2, 3}
-    """
-    B, T, D = audio_feat.shape
-    device = audio_feat.device
+def contrastive_loss_with_mask(flat_feat, flat_mask):
+    device = flat_feat.device
 
-    flat_feat = audio_feat.reshape(-1, D)         # [B*T, D]
-    flat_mask = mask.reshape(-1)                  # [B*T]
-
-    valid_mask = flat_mask != 3                   # 제거할 영역 제외
-    flat_feat = flat_feat[valid_mask]             # [N, D]
-    flat_mask = flat_mask[valid_mask]             # [N]
-
-    if flat_feat.size(0) == 0:
-        return torch.tensor(0.0, requires_grad=True, device=device)
+    # mask == 3 제거
+    valid_mask = flat_mask != 3
+    flat_feat = flat_feat[valid_mask]
+    flat_mask = flat_mask[valid_mask]
 
     flat_feat = F.normalize(flat_feat, dim=1)
 
-    pos_strong_idx = (flat_mask == 2).nonzero(as_tuple=False).squeeze(1)
-    pos_weak_idx = (flat_mask == 1).nonzero(as_tuple=False).squeeze(1)
-    neg_idx = (flat_mask == 0).nonzero(as_tuple=False).squeeze(1)
+    pos_strong_idx = torch.nonzero(flat_mask == 2).squeeze(1)
+    pos_weak_idx = torch.nonzero(flat_mask == 1).squeeze(1)
+    neg_idx = torch.nonzero(flat_mask == 0).squeeze(1)
 
     if len(pos_strong_idx) == 0 or len(neg_idx) == 0:
         return torch.tensor(0.0, requires_grad=True, device=device)
 
     pos_strong_feat = flat_feat[pos_strong_idx]
+    pos_weak_feat = flat_feat[pos_weak_idx] if len(pos_weak_idx) > 0 else None
     neg_feat = flat_feat[neg_idx]
 
     sim_strong = torch.matmul(pos_strong_feat, neg_feat.T) / TEMPERATURE
     loss_strong = -F.log_softmax(sim_strong, dim=1).mean()
 
-    if len(pos_weak_idx) > 0:
-        pos_weak_feat = flat_feat[pos_weak_idx]
+    if pos_weak_feat is not None:
         sim_weak = torch.matmul(pos_weak_feat, neg_feat.T) / TEMPERATURE
         loss_weak = -F.log_softmax(sim_weak, dim=1).mean()
     else:
@@ -47,3 +37,4 @@ def contrastive_loss_with_mask(audio_feat, mask):
 
     total_loss = WEIGHT_STRONG_POS * loss_strong + WEIGHT_WEAK_POS * loss_weak
     return total_loss
+
