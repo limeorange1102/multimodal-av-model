a12d41282c12f0e59f920333a2d1caa57a017ab2
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-30 02:47:06 +0900

    sibal

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -4,24 +4,26 @@ class Tokenizer:
     def __init__(self, vocab_path):
         self.token_to_id = {}
         self.id_to_token = []
-        
+
         with open(vocab_path, 'r', encoding='utf-8') as f:
             for idx, line in enumerate(f):
-                token = line.strip().split('\t')[0]  # sentencepiece .vocab format
+                token = line.strip().split('\t')[0]  # sentencepiece .vocab 형식
                 self.token_to_id[token] = idx
                 self.id_to_token.append(token)
 
     def encode(self, text):
-        # 공백은 SentencePiece가 '▁'로 처리하므로 수동으로 대응
         return [
             self.token_to_id.get(ch if ch != ' ' else '▁', self.unk_id)
             for ch in text
         ]
 
     def decode(self, ids):
-        tokens = [self.id_to_token[i] for i in ids if i < len(self.id_to_token)]
+        tokens = [
+            self.id_to_token[i] for i in ids
+            if 0 <= i < len(self.id_to_token)
+        ]
         return ''.join(tokens).replace('▁', ' ').strip()
-    
+
     @property
     def vocab_size(self):
         return len(self.id_to_token)
