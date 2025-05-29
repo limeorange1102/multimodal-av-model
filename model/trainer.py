2a0b3b2ce8a1babfac6298a1f47a4781c25ae2b3
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-30 06:13:44 +0900

    tlqkf

--- model/trainer.py
+++ model/trainer.py
@@ -83,11 +83,10 @@ class MultimodalTrainer:
                 audio_feat1 = self.audio_encoder(audio, attention_mask=attn_mask1)
                 audio_feat2 = self.audio_encoder(audio, attention_mask=attn_mask2)
                 # mask1도 attention mask를 기반으로 압축
-                mask1_compact = torch.cat([m[attn_mask1[i]] for i, m in enumerate(mask1)], dim=0)
-                mask2_compact = torch.cat([m[attn_mask2[i]] for i, m in enumerate(mask2)], dim=0)
-                # audio_feat도 동일하게 flatten
-                audio_feat1_flat = torch.cat([a[attn_mask1[i]] for i, a in enumerate(audio_feat1)], dim=0)
-                audio_feat2_flat = torch.cat([a[attn_mask2[i]] for i, a in enumerate(audio_feat2)], dim=0)
+                mask1_compact = torch.cat([m[m_].long() for m, m_ in zip(mask1, attn_mask1)], dim=0)  # [N]
+                mask2_compact = torch.cat([m[m_].long() for m, m_ in zip(mask2, attn_mask2)], dim=0)  # [N]
+                audio_feat1_flat = torch.cat([a[m] for a, m in zip(audio_feat1, attn_mask1)], dim=0)  # [N, D]
+                audio_feat2_flat = torch.cat([a[m] for a, m in zip(audio_feat2, attn_mask2)], dim=0)  # [N, D]
                 loss_contrast1 = contrastive_loss_with_mask(audio_feat1_flat, mask1_compact)
                 loss_contrast2 = contrastive_loss_with_mask(audio_feat2_flat, mask2_compact)
                 fused_feat1 = self.fusion_module(visual_feat1, audio_feat1)
