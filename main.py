c8ee259e963f8b37d5ad4b98b413334414b705da
Hyeonu Yu <limeorange1102@gmail.com>
2025-05-30 22:02:15 +0900

    tlqkf

--- main.py
+++ main.py
@@ -67,9 +67,9 @@ def main():
     set_seed()
 
     json_folder = "input_texts"
-    npy_dir = "processed_dataset/npy"
+    npy_dir = "npy"
     text_dir = "processed_dataset/text"
-    wav_dir = "input_videos"
+    wav_dir = "input_wav/input_wav"
 
     tokenizer = Tokenizer(vocab_path="input_videos/tokenizer800.vocab")
     sentence_list = build_data_list(json_folder, npy_dir, text_dir, wav_dir)    
