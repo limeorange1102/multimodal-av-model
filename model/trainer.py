ed7ef7b28882f13cfb6935300f1a7400e710a37b
Hyeonu Yu <limeorange1102@gmail.com>
2025-06-01 11:38:19 +0900

    tlqkf rowehoTek

--- model/trainer.py
+++ model/trainer.py
@@ -82,16 +82,16 @@ class MultimodalTrainer:
 
                     visual_feat1 = self.visual_encoder(lip1)
                     visual_feat2 = self.visual_encoder(lip2)
-                    
+
                     attn_mask1 = (mask1 != 3)
                     attn_mask2 = (mask2 != 3)
                     speach_mask1 = (mask1 != 0) & (mask1 != 3)  # [B, T_audio]
                     speach_mask2 = (mask2 != 0) & (mask2 != 3)
 
-                    audio_feat1 = self.audio_encoder(audio, attention_mask=speach_mask1)
-                    audio_feat1_middle = self.audio_encoder(audio, attention_mask=attn_mask1)
-                    audio_feat2 = self.audio_encoder(audio, attention_mask=speach_mask2)
-                    audio_feat2_middle = self.audio_encoder(audio, attention_mask=attn_mask2)
+                    audio_feat1, _ = self.audio_encoder(audio, attention_mask=speach_mask1)
+                    _, audio_feat1_middle = self.audio_encoder(audio, attention_mask=attn_mask1)
+                    audio_feat2, _ = self.audio_encoder(audio, attention_mask=speach_mask2)
+                    _, audio_feat2_middle = self.audio_encoder(audio, attention_mask=attn_mask2)
 
                     B, T_enc, D = audio_feat1.shape
                     mask1_ds = F.interpolate(mask1.unsqueeze(1).float(), size=T_enc, mode='nearest').squeeze(1).long()  # [B, T_enc]
@@ -204,14 +204,14 @@ class MultimodalTrainer:
                 mask2 = batch["mask2"].to(self.device)
 
                 visual_feat1 = self.visual_encoder(lip1)   
-                attn_mask1 = (mask1 != 3)     #Boolean mask for attention
+                attn_mask1 = (mask1 != 3) & (mask1 != 0)     #Boolean mask for attention
                 audio_feat1, _ = self.audio_encoder(audio, attention_mask=attn_mask1)
                 fused_feat1 = self.fusion_module(visual_feat1, audio_feat1)
                 log_probs1 = self.decoder1(fused_feat1) #(log_probs1.shape: [B, T, V])
                 log_probs1 = F.log_softmax(log_probs1, dim=-1)  # log softmax for CTC
 
                 visual_feat2 = self.visual_encoder(lip2)
-                attn_mask2 = (mask2 != 3)
+                attn_mask2 = (mask2 != 3) & (mask2 != 0)  # Boolean mask for attention
                 audio_feat2, _ = self.audio_encoder(audio, attention_mask=attn_mask2)
                 fused_feat2 = self.fusion_module(visual_feat2, audio_feat2)
                 log_probs2 = self.decoder1(fused_feat2)
